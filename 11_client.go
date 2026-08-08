c4c340896e92d0f94bae5157444eb338a31e0983
Eleanor McHugh <eleanor@games-with-brains.com>
2026-08-08 01:43:27 +0100

    concurrently process multiple resources listed on the command line using a sync.WaitGroup

---
+++ 11_client.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"sync"
+	"time"
+)
+
+const FETCH_ALL_RESULTS = -1
+const HREF = `(i?)href[:blank:]*=[:blank:]*"[^"]*"`
+const WEB_DOMAIN = `(i?)https?://.*/?`
+
+const TIMEOUT = 3
+
+const (
+	E_OK = iota
+	E_FILE_READ
+)
+
+var W *regexp.Regexp
+
+func init() {
+	W = regexp.MustCompile(WEB_DOMAIN)
+}
+
+func main() {
+	H := regexp.MustCompile(HREF)
+
+	ProcessTargets(func(n string, b ...byte) {
+		ProcessText(H, n, b...)
+	})
+	os.Exit(E_OK)
+}
+
+func ProcessTargets(f func(string, ...byte)) {
+	if len(os.Args) > 0 {
+		var wg sync.WaitGroup
+		for _, fn := range os.Args[1:] {
+			wg.Go(func() {
+				if W.MatchString(fn) {
+					ForServer(fn, f)
+				} else {
+					ForFile(fn, f)
+				}
+			})
+		}
+		wg.Wait()
+	}
+	return
+}
+
+func ForServer(url string, f func(string, ...byte)) {
+	log.Printf("Load Web Page: %v", url)
+	c := http.Client{Timeout: time.Duration(TIMEOUT) * time.Second}
+	ExitOnError(E_FILE_READ, func() (e error) {
+		if r, e := c.Get(url); e == nil {
+			ReadStream(r.Body, func(b ...byte) {
+				f(url, b...)
+			})
+		}
+		return
+	})
+}
+
+func ForFile(n string, f func(string, ...byte)) {
+	log.Printf("Load File: %v", n)
+	ExitOnError(E_FILE_READ, func() (e error) {
+		if b, e := os.ReadFile(n); e == nil {
+			f(n, b...)
+		}
+		return
+	})
+}
+
+func ProcessText(r *regexp.Regexp, n string, b ...byte) {
+	if r.MatchString(string(b)) {
+		s := r.FindAllStringIndex(string(b), FETCH_ALL_RESULTS)
+		log.Printf("%v: Found pattern %v times", n, len(s))
+
+		for _, v := range s {
+			log.Printf("%v: Pattern matched: %v", n, string(b[v[0]:v[1]]))
+		}
+	}
+}
+
+func ReadStream(in io.ReadCloser, f func(...byte)) (e error) {
+	defer in.Close()
+	b, e := io.ReadAll(in)
+	if e == nil {
+		f(b...)
+	} else {
+		log.Print(e)
+	}
+	return
+}
+
+func ExitOnError(ec int, f func() error) {
+	if e := f(); e != nil {
+		log.Print(e)
+		os.Exit(ec)
+	}
+}
