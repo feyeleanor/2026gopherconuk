efc0aa58c11faf6fa0ef6556ef8a973cec458f47
Eleanor McHugh <eleanor@games-with-brains.com>
2026-08-08 01:17:51 +0100

    read text from a file named on the command line and check it for an instance of a regular expression

---
+++ 02_regex.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+	"regexp"
+)
+
+const REGEX = `(i?)href[:blank:]*=[:blank:]*"[^"]*"`
+
+const (
+	E_OK = iota
+	E_FILE_READ
+)
+
+func main() {
+	//	compile Regex
+	r := regexp.MustCompile(REGEX)
+	log.Print(r)
+
+	//	check for a filename as a parameter
+	if len(os.Args) > 0 {
+		//	get the filename
+		fn := os.Args[1]
+
+		//	load the file
+		if b, e := os.ReadFile(fn); e == nil {
+			ProcessText(r, b...)
+		} else {
+			os.Exit(E_FILE_READ)
+		}
+	}
+
+	os.Exit(E_OK)
+}
+
+func ProcessText(r *regexp.Regexp, b ...byte) {
+	if r.MatchString(string(b)) {
+		i := r.FindStringIndex(string(b))
+		log.Printf("Found pattern at: %v", i)
+		log.Printf("Pattern matched: %v", string(b[i[0]:i[1]]))
+	}
+}
+
+
+// string(b[71:110])
